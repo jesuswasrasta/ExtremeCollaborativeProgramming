d940d52c7467695a2d2c23cbd35fdb1a38819155
Ferdinando Santacroce <ferdinando.santacroce@gmail.com>
2024-02-26 12:40:20 +0100

    C# boilerplate

---
+++ csharp/Kata.Tests/Tests.cs
@@ -0,0 +1,18 @@
+﻿using NUnit.Framework;
+
+namespace Kata.Tests
+{
+    public class Tests
+    {
+        [SetUp]
+        public void Setup()
+        {
+        }
+
+        [Test]
+        public void ShouldPass()
+        {
+            Assert.Pass();
+        }
+    }
+}
